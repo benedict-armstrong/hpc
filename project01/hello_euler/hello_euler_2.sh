6c8e0eb0256ddefad2d0f01914ffa04b8368a3cf
Benedict Armstrong <barmstron@student.ethz.ch>
2024-03-07 12:38:48 +0100

    1a.1 completed

--- project01/hello_euler/hello_euler_2.sh
+++ project01/hello_euler/hello_euler_2.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=hello_euler      # Job name    (default: sbatch)
-#SBATCH --output=hello_euler.out # Output file (default: slurm-%j.out)
-#SBATCH --error=hello_euler.err  # Error file  (default: slurm-%j.out)
+#SBATCH --job-name=hello_euler_2      # Job name    (default: sbatch)
+#SBATCH --output=hello_euler_2.out    # Output file (default: slurm-%j.out)
+#SBATCH --error=hello_euler_2.err     # Error file  (default: slurm-%j.out)
 #SBATCH --time=00:01:00               # Wall clock time limit
-#SBATCH --nodes=2                    # Number of tasks
-#SBATCH --ntasks=1                    # Number of tasks
+#SBATCH --nodes=2                     # Number of tasks
+#SBATCH --ntasks=2                    # Number of tasks
 #SBATCH --cpus-per-task=1             # Number of CPUs per task
 #SBATCH --mem-per-cpu=1024            # Memory per CPU
-#SBATCH --constraint=EPYC_9654
 
-srun hello_euler
+srun hello_euler hello_euler
