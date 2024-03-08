300145a1e9b2db33199d156985c8bb2ed50c2d51
Benedict Armstrong <barmstron@student.ethz.ch>
2024-03-08 15:36:44 +0100

    run STREAM benchmark

--- project01/01a/stream_benchmark/run.sh
+++ project01/01a/stream_benchmark/run.sh
@@ -1,4 +1,4 @@
 module load gcc # load a compiler (gcc/11.4.0)
-gcc -O3 -march=native -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=10000000 \
+gcc -O3 -march=native -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=20000000 \
     -DNTIMES=20 stream.c -o stream_c.exe        # compile
-sbatch --mem-per-cpu=2G --wrap "./stream_c.exe" # submit to queue and run
+sbatch --output=EPYC_7763.out --constraint=EPYC_7763 --mem-per-cpu=2G --wrap "./stream_c.exe" # submit to queue and run
