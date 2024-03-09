53d7cb5f34cab8a29b9f58083b901a91592d15a5
Benedict Armstrong <barmstron@student.ethz.ch>
2024-03-09 15:34:55 +0100

    matr mult runs

--- project01/01b/dgemm-blocked.c
+++ project01/01b/dgemm-blocked.c
@@ -1,5 +1,9 @@
 const char *dgemm_desc = "Blocked dgemm.";
 
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 32
+#endif
+
 /* This routine performs a dgemm operation
  *
  *  C := C + A * B
@@ -8,18 +12,14 @@ const char *dgemm_desc = "Blocked dgemm.";
  * On exit, A and B maintain their input values.
  */
 void square_dgemm(int n, double *A, double *B, double *C) {
-  // TODO: Implement the blocking optimization
-  //       (The following is a placeholder naive three-loop dgemm)
 
-  // Blocked dgemm
-  int block_size = 100;
-  for (int i = 0; i < n; i += block_size) {
-    for (int j = 0; j < n; j += block_size) {
-      for (int k = 0; k < n; k += block_size) {
+  for (int i = 0; i < n; i += BLOCK_SIZE) {
+    for (int j = 0; j < n; j += BLOCK_SIZE) {
+      for (int k = 0; k < n; k += BLOCK_SIZE) {
         // Correct block dimensions if block "goes off edge of" the matrix
-        int M = (i + block_size < n) ? block_size : n - i;
-        int N = (j + block_size < n) ? block_size : n - j;
-        int K = (k + block_size < n) ? block_size : n - k;
+        int M = (i + BLOCK_SIZE < n) ? BLOCK_SIZE : n - i;
+        int N = (j + BLOCK_SIZE < n) ? BLOCK_SIZE : n - j;
+        int K = (k + BLOCK_SIZE < n) ? BLOCK_SIZE : n - k;
         // Perform individual block dgemm
         for (int i2 = 0; i2 < M; ++i2) {
           for (int j2 = 0; j2 < N; ++j2) {
