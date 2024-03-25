2c8ef60c283ba2cf6b5c686193ff24bf59af5543
Benedict Armstrong <contact@benarmstro.ng>
2024-03-25 17:57:14 +0100

    report

---
+++ project02/create_submission.sh
@@ -0,0 +1,6 @@
+mkdir -p "02_armstrong_benedict"
+cp -r "code" "02_armstrong_benedict/"
+cp "report/02_armstrong_benedict.pdf" "02_armstrong_benedict/02_armstrong_benedict.pdf"
+
+zip -r "02_armstrong_benedict.zip" "02_armstrong_benedict"
+rm -r "02_armstrong_benedict"
