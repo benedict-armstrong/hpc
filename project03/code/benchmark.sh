fd02d54234123f6634fd1af26478585c617b9730
Benedict Armstrong <barmstron@student.ethz.ch>
2024-04-12 10:22:46 +0200

    run strong 20 runs

--- project03/code/benchmark.sh
+++ project03/code/benchmark.sh
@@ -6,7 +6,7 @@
 #SBATCH --constraint=EPYC_7763   # Select node with CPU
 #SBATCH --cpus-per-task=16        # Number of CPUs per task
 #SBATCH --mem-per-cpu=1000       # Memory per CPU
-#SBATCH --time=01:00:00          # Wall clock time limit
+#SBATCH --time=02:00:00          # Wall clock time limit
 
 # Load some modules & list loaded modules
 module load gcc python
@@ -18,4 +18,4 @@ make clean
 make
 
 # Run the program
-srun python3 strong_benchmark.py --sizes 64 128 256 512 1024 --threads 1 2 4 8 16 --runs 5
+srun python3 strong_benchmark.py --sizes 64 128 256 512 1024 --threads 1 2 4 8 16 --runs 20
