ff762c3bcd992cd420252887045a77df70812291
Benedict Armstrong <contact@benarmstro.ng>
2024-04-13 16:44:19 +0200

    updated run config

--- project03/code/benchmark_weak.sh
+++ project03/code/benchmark_weak.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1               # Number of tasks
 #SBATCH --constraint=EPYC_7763   # Select node with CPU
 #SBATCH --cpus-per-task=64        # Number of CPUs per task
-#SBATCH --mem-per-cpu=1000       # Memory per CPU
+#SBATCH --mem-per-cpu=4000       # Memory per CPU
 #SBATCH --time=10:00:00          # Wall clock time limit
 
 # Load some modules & list loaded modules
