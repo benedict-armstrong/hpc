d2e7d071e3e81c626e41c67a7468a4273bfbf08f
Benedict Armstrong <contact@benarmstro.ng>
2024-04-13 16:43:22 +0200

    cleanup and report

--- project03/code/strong_benchmark.py
+++ project03/code/strong_benchmark.py
@@ -2,8 +2,6 @@ import argparse
 import json
 import re
 import subprocess
-from typing import List
-import matplotlib.pyplot as plt
 
 
 def run(n_threads: int, size=128, times_steps=100, final_time=0.005) -> float:
@@ -70,8 +68,6 @@ if __name__ == "__main__":
         "--runs", help="Number of runs", type=int, default=3)
     argparser.add_argument(
         "--threads", help="Number of threads to run the benchmark for", type=int, nargs="+", default=[1, 2, 4, 8])
-    argparser.add_argument(
-        "--plot_from", help="Plot from the given file", type=str, default=None)
 
     args = argparser.parse_args()
 
@@ -79,95 +75,28 @@ if __name__ == "__main__":
     sizes = args.sizes
     runs = args.runs
 
-    if not args.plot_from:
-        # print config
-        print(f"Running benchmark for sizes: {sizes}")
-        print(f"Running benchmark for threads: {threads}")
-        print(f"Number of runs: {runs}")
+    # print config
+    print(f"Running benchmark for sizes: {sizes}")
+    print(f"Running benchmark for threads: {threads}")
+    print(f"Number of runs: {runs}")
 
-        times = []
+    times = []
 
-        for size in sizes:
-            temp = []
-            print(f"Running benchmark for size {size}")
-            for thread in threads:
-                r = []
-                for _ in range(runs):
-                    r.append(run(thread, size))
-                temp.append(r)
-            times.append(temp)
+    for size in sizes:
+        temp = []
+        print(f"Running benchmark for size {size}")
+        for thread in threads:
+            r = []
+            for _ in range(runs):
+                r.append(run(thread, size))
+            temp.append(r)
+        times.append(temp)
 
-        # save time results to a json file
-        data = {
-            "threads": threads,
-            "sizes": sizes,
-            "runs": runs,
-            "times": times,
-        }
-        json.dump(data, open("strong_scaling.json", "w"))
-
-    else:
-
-        print(f"Plotting from {args.plot_from}")
-
-        with open(args.plot_from, "r") as f:
-            data = json.load(f)
-
-    # plot each threadcount as a line (different color) and each size as a point with error bars
-    plt.Figure(figsize=(15, 10))
-
-    for i, size in enumerate(data["sizes"]):
-        times_for_size = []
-
-        for t in data["times"][i]:
-            times_for_size.append(t[1:])
-
-        plt.errorbar(
-            data["threads"],
-            [sum(t) / len(t) for t in times_for_size],
-            yerr=[max(t) - min(t) for t in times_for_size],
-            label=f"size {size}",
-            capsize=3,
-        )
-
-    plt.xlabel("Number of OpenMP threads")
-    plt.yscale("log")
-    plt.ylabel("Time [s]")
-    plt.grid()
-    plt.legend()
-
-    plt.title(f"Strong benchmark ({data['runs']} runs, first removed)")
-
-    plt.savefig("../report/plots/strong_scaling.pdf")
-
-    plt.clf()
-
-    # plot speedup
-
-    for i, size in enumerate(data["sizes"]):
-
-        times_for_size = []
-
-        for t in data["times"][i]:
-            times_for_size.append(t[1:])
-
-        t_1 = [sum(t) / len(t) for t in times_for_size][0]
-        x_values = data["threads"]
-        y_values = [t_1 / (sum(t) / len(t)) for t in times_for_size]
-        plt.errorbar(
-            x_values,
-            y_values,
-            yerr=[max(t) - min(t) for t in times_for_size],
-            capsize=3,
-            label=f"size {size}")
-
-    plt.plot(data["threads"], data["threads"], "--", label="ideal speedup")
-
-    plt.xlabel("Number of OpenMP threads")
-    plt.ylabel("Speedup")
-    plt.grid()
-    plt.legend()
-
-    plt.title(f"Strong benchmark speedup ({data['runs']} runs, first removed)")
-
-    plt.savefig("../report/plots/strong_speedup.pdf")
+    # save time results to a json file
+    data = {
+        "threads": threads,
+        "sizes": sizes,
+        "runs": runs,
+        "times": times,
+    }
+    json.dump(data, open("strong_scaling.json", "w"))
