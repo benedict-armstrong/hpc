45876f0f1c8b759609350b3c1610619cd94486ea
Benedict Armstrong <contact@benarmstro.ng>
2024-04-29 23:27:02 +0200

    [p4] almost final

--- project04/create_submission.sh
+++ project04/create_submission.sh
@@ -3,5 +3,5 @@ cp -r "code" "04_armstrong_benedict/"
 cp -r "report/plots" "04_armstrong_benedict/plots"
 cp "report/04_armstrong_benedict.pdf" "04_armstrong_benedict/04_armstrong_benedict.pdf"
 
-zip -r "04_armstrong_benedict.zip" "04_armstrong_benedict" -x ./04_armstrong_benedict/code/build/\* ./04_armstrong_benedict/code/.gitignore
+zip -r "04_armstrong_benedict.zip" "04_armstrong_benedict" -x ./04_armstrong_benedict/code/\*/build/\* ./04_armstrong_benedict/code/.gitignore
 rm -r "04_armstrong_benedict"
