93829bfcba68f83756b86bccaa6dc5f15264401d
Benedict Armstrong <contact@benarmstro.ng>
2024-04-29 12:06:51 +0200

    [p4] added task-per-cpu

--- project04/code/powermethod/benchmark_strong.sh
+++ project04/code/powermethod/benchmark_strong.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=power_strong      # Job name
-#SBATCH --output=out/power_strong.out # Output file
-#SBATCH --error=out/power_strong.err  # Error file
-#SBATCH --ntasks=64               # Number of tasks
-#SBATCH --nodes=64              # Number of nodes
-#SBATCH --constraint=EPYC_7763   # Select node with CPU
-#SBATCH --cpus-per-task=1        # Number of CPUs per task
-#SBATCH --mem-per-cpu=1000       # Memory per CPU
-#SBATCH --time=10:00:00          # Wall clock time limit
+#SBATCH --job-name=power_strong         # Job name
+#SBATCH --output=out/power_strong.out   # Output file
+#SBATCH --error=out/power_strong.err    # Error file
+#SBATCH --ntasks=64                     # Number of tasks
+#SBATCH --nodes=64                      # Number of nodes
+#SBATCH --ntasks-per-node=1             # Number of tasks per node
+#SBATCH --cpus-per-task=1               # Number of CPUs per task
+#SBATCH --mem-per-cpu=950               # Memory per CPU
+#SBATCH --constraint=EPYC_7763          # Select node with CPU
+#SBATCH --time=10:00:00                 # Wall clock time limit
 
 # Load some modules & list loaded modules
 module load gcc python openmpi
