44b3cb311e7e0c514489e30a136db9eb1db24496
Benedict Armstrong <barmstron@student.ethz.ch>
2024-05-09 11:39:38 +0200

    [p5] py manager

--- project04/code/powermethod/benchmark_strong_multi.sh
+++ project04/code/powermethod/benchmark_strong_multi.sh
@@ -22,4 +22,4 @@ make clean
 make
 
 # Run the program
-python3 benchmark.py --runs 15 --processes 1 2 4 8 16 32 64 --type strong --base_size 10000 --name multi_node
+python3 benchmark.py --runs 15 --processes 64 --type strong --base_size 10000 --name multi_node
--- project05/code/hpc_python/ManagerWorker/benchmark.sh
+++ project05/code/hpc_python/ManagerWorker/benchmark.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
-#SBATCH --job-name=manager_worker      # Job name    (default: sbatch)
-#SBATCH --output=manager_worker.out # Output file (default: slurm-%j.out)
-#SBATCH --error=manager_worker.err  # Error file  (default: slurm-%j.out)
-#SBATCH --nodes=1                 # Number of nodes
-#SBATCH --ntasks=33               # Number of tasks
-#SBATCH --constraint=EPYC_7763    # Select node with CPU
-#SBATCH --cpus-per-task=1         # Number of CPUs per task
-#SBATCH --mem-per-cpu=1024        # Memory per CPU
-#SBATCH --time=00:10:00           # Wall clock time limit
+#SBATCH --job-name=manager_worker                       # Job name    (default: sbatch)
+#SBATCH --output=manager_worker.out                     # Output file (default: slurm-%j.out)
+#SBATCH --error=manager_worker.err                      # Error file  (default: slurm-%j.out)
+#SBATCH --nodes=1                                       # Number of nodes
+#SBATCH --ntasks=33                                     # Number of tasks
+#SBATCH --constraint=EPYC_7763                          # Select node with CPU
+#SBATCH --cpus-per-task=1                               # Number of CPUs per task
+#SBATCH --mem-per-cpu=1024                              # Memory per CPU
+#SBATCH --time=00:10:00                                 # Wall clock time limit
+#SBATCH --mail-user=benedict.armstrong@inf.ethz.ch
+#SBATCH --mail-type=END
 
 # Load some modules & list loaded modules
 module load gcc openmpi python
 module list
 
-python -m venv project05-env      # creates a virtual environment
+# Create a virtual environment
+python3 -m venv --system-site-packages project05-env
 source project05-env/bin/activate # activates the virtual environment
+
+# install required packages
 pip install numpy scipy matplotlib mpi4py
 
+# list installed packages
+pip list
+
+runs=10
+
+# add header to the output file
+echo "workers,tasks,time,run" >out/results.csv
+
 # run with 3-33 workers and 100/50 tasks
-
-for i in {3..33}; do
-    echo "Running with $i workers and 50 tasks"
-    (mpirun -np $i python3 manager_worker.py 4001 4001 50) | grep "Run took" >>manager_worker_results_50.txt
-done
-
-for i in {3..33}; do
-    echo "Running with $i workers and 100 tasks"
-    (mpirun -np $i python3 manager_worker.py 4001 4001 100) | grep "Run took" >>manager_worker_results_100.txt
+for task in 100 50; do
+    for i in {3..33}; do
+        echo "Running with $i workers and $task tasks and $runs runs"
+        for j in $(seq 1 $runs); do
+            time=$(mpirun -np $i python3 manager_worker.py 4001 4001 $task | grep "Run took" | awk '{print $3}')
+            echo "$i,$task,$time,$j" >>out/results.csv
+        done
+    done
 done
 
 deactivate
 echo "Done"
+
